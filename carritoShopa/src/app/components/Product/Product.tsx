777757807158208ab6f3dfeec26b998d1b882a50
LucasEscribe <lfd.meneses@gmail.com>
2023-06-03 12:49:01 -0300

    Secciones esquematizadas. Lógica planteada. Cero CSS.

--- carritoShopa/src/app/components/Product/Product.tsx
+++ carritoShopa/src/app/components/Product/Product.tsx
@@ -1,21 +1,51 @@
-import styles from "./artist.module.css";
+import styles from "./Product.module.css";
+import { useState } from "react";
 
-type ArtistProps = {
-  img: string
+type ProductProps = {
+  id: string;
   name: string;
-  song: string;
-  views: string | number;
+  description: string;
+  price: string | number;
+  updateTotal: (subtotal: number) => void;
+};
+
+function Product(props: ProductProps) {
+  const [quantity, setQuantity] = useState(0);
+
+  const handleAddUnit = () => {
+    setQuantity(quantity + 1);
+    calculateSubtotal(quantity + 1);
+  };
+
+  const handleRemoveUnit = () => {
+    if (quantity > 0) {
+      setQuantity(quantity - 1);
+      calculateSubtotal(quantity - 1);
+    }
+  };
+
+  const calculateSubtotal = (newQuantity: number) => {
+    const subtotal = newQuantity * Number(props.price);
+    props.updateTotal(subtotal);
+  };
+
+  return (
+    <div className={styles.product}>
+      <h2 className={styles.name}>{props.name}</h2>
+      <p className={styles.description}>{props.description}</p>
+      <p className={styles.price}>{props.price}</p>
+
+      <div className={styles.unitContainer}>
+        <button className={styles.unitButton} onClick={handleRemoveUnit}>
+          -
+        </button>
+        <span className={styles.quantity}>{quantity}</span>
+        <button className={styles.unitButton} onClick={handleAddUnit}>
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
 
-function Artist(props: ArtistProps) {
-    return (
-      <div className={styles.center}>
-          <img src={props.img} />
-          <h2 className='artist'>{props.name}</h2>
-          <p className="song">{props.song}</p>
-          <p className='views'>{props.views}</p>
-      </div>
-    );
-  }
-
-  export default Artist;
+export default Product;
--- carritoShopa/src/app/screens/Home/index.tsx
+++ carritoShopa/src/app/screens/Home/index.tsx
@@ -1,21 +1,67 @@
-import Product from '../../components/Product/Product.tsx';
-//import styles from './styles.modules.css';
-import './styles.modules.css'
+import { useState } from "react";
+import "../../../global.css";
+
+import Product from "../../components/Product/Product.tsx";
+import LoadProduct from "../../components/Product/LoadProduct.tsx";
+import Total from "../../components/Cart/Total.tsx";
 
 function App() {
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const updateTotal = (subtotal: number) => {
+    setTotalPrice(totalPrice + subtotal); 
+  };
+
   return (
     <main>
-      <div className="side"></div>
-
-      <div className="center">
-        <h1 className="section1">Recently Played</h1>
-        <br />
-        <Product img="/src/app/assets/img/Productas/colchones/colchones_opa.jpg" name="colchones" song="Opa" views="10M" />
-        <Product img="/src/app/assets/img/Productas/colchones/colchones_pelotuda.jpg" name="colchones" song="Pelotuda" views="18M" />
-        <Product img="/src/app/assets/img/Productas/colchones/colchones_sauce.jpg" name="colchones" song="Sauce" views="6M" />
+      <div className="side_left">
+        <h1 className="title_left">Cargar Producto</h1>
+        <LoadProduct />
       </div>
 
-      <div className="bottom"></div>
+      <div className="center">
+        <h1 className="title_center">Listado</h1>
+        <Product
+          id="1001"
+          name="Colchón Kamikase"
+          description="Duerma o Muera en den 50"
+          price={224000}
+          updateTotal={updateTotal}
+        />
+        <Product
+          id="1002"
+          name="Colchón Químico"
+          description="Ignífugo para fumadores den 30"
+          price={250000}
+          updateTotal={updateTotal}
+        />
+        <Product
+          id="1003"
+          name="Colchón Pasti"
+          description="La cura del dolor den 100"
+          price={375000}
+          updateTotal={updateTotal}
+        />
+        <Product
+          id="1004"
+          name="Colchón Amor"
+          description="Duerma solo den 1"
+          price={20000}
+          updateTotal={updateTotal}
+        />
+        <Product
+          id="1005"
+          name="Colchón Party"
+          description="Hasta 30 en den 4"
+          price={1250000}
+          updateTotal={updateTotal}
+        />
+      </div>
+
+      <div className="side_right">
+        <h2 className="title_right">Total</h2>
+        <Total totalPrice={totalPrice} />
+      </div>
     </main>
   );
 }
